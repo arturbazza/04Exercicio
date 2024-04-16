08d714563dfc1be49fd989cfbc1209a2c8cb7ce0
arturbazza <agencia@fonzie.com.br>
2024-04-16 16:58:06 -0300

    Projeto vazio

---
+++ src/test/java/br/org/fundatec/atividade04/Atividade04ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.fundatec.atividade04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
