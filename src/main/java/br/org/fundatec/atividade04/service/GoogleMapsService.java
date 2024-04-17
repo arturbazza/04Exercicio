5ab7712026926a97be0d0ba46029e0c44b2869a7
arturbazza <agencia@fonzie.com.br>
2024-04-16 21:05:23 -0300

    Retornando latitude, longitude ZERO

--- src/main/java/br/org/fundatec/atividade04/service/GoogleMapsService.java
+++ src/main/java/br/org/fundatec/atividade04/service/GoogleMapsService.java
@@ -23,6 +23,7 @@ public class GoogleMapsService {
         this.restTemplate = restTemplate;
     }
 
+    // https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDgJSB5HA4p8Yuw0zq3qrloSLk79JCodc8&address=Av.%20Jer%C3%B4nimo%20de%20ornelas,%20115,%20porto%20alegre,%20rs
     public Optional<GeographicLocation> getGeographicLocation(String address) {
         String url = "https://maps.googleapis.com/maps/api/geocode/json?key=" + apiKey + "&address=" + address;
 
